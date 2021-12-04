c66d444594eb74b8d88c5aecf25863fde6803f0d
Prateek Garg <prateekgarg0020@gmail.com>
2021-12-05 03:28:47 +0530

    Convergence metric

--- Experiment.m
+++ Experiment.m
@@ -4,51 +4,66 @@ format ShortG ;
 
 t_samp=5; %track-to-track interval
 n=5000; %Number of Samples
+epsilon=0.01;
+
 t=linspace(t_samp,t_samp*n,n); %time values
 %% ==================== Static System ====================
-% true_val=1000;
-% [X,V,A]=System(true_val,0,0,t_samp,n);
-% Z = X+randn(1,n);
-% 
-% alpha=1:n;
-% alpha=1./alpha;
-% 
-% val_guess=998;
-% 
-% [x,v,a,x_est,v_est,a_est]=Filter(alpha,0,0,t_samp,Z,val_guess,0,0);
-% 
-% [t./t_samp;Z;x] %#ok<NOPTS> 
-% figure
-% plot(t,X,t,Z,t,x,'linewidth',1.5);
-% legend("True Value","Measurements","Estimates");
-% fprintf('Program paused. Press enter to continue.\n');
-% pause;
+true_val=1000;
+[X,~,~]=System(true_val,0,0,t_samp,n);
+Z = X+randn(1,n);
 
+alpha=1:n;
+alpha=1./alpha;
+
+val_guess=998;
+
+[x,~,~,~,~,~]=Filter(alpha,0,0,t_samp,Z,val_guess,0,0);
+
+%[t./t_samp;Z;x] %#ok<NOPTS> 
+figure
+plot(t,X,t,Z,t,x,'linewidth',1.5);
+legend("True Value","Measurements","Estimates");
+
+E=((X-x)./X)*100;
+figure
+plot(t,E,'linewidth',1.5);
+legend("Error Percentage");
+
+disp(find(E>=epsilon,1,'last'));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+close all;
 %% ======================= Constant Velocity =======================
-% x_init=30000;
-% v_init=40;
-% [X,V,A]=System(x_init,v_init,0,t_samp,n);
-% 
-% Z = X+400*randn(1,n);
-% 
-% alpha=0.2;
-% beta=0.1;
-% 
-% x_guess=30000;
-% v_guess=40;
-% %Filter(alpha,beta,gamma,t,z ,x_guess,v_guess,a_guess)
-% [x,v,a,x_est,v_est,a_est]=Filter(alpha,beta,0,t_samp,Z,x_guess,v_guess,0);
-% 
-% [t./t_samp;X;Z;x;v;x_est;v_est] %#ok<NOPTS> 
-% figure
-% plot(t,X,t,Z,t,x,t,x_est,'linewidth',1.5);
-% %plot(t,X,'linewidth',1.5);
-% 
-% legend("True Value","Measurements","Estimates","Predictions");
-% 
-% fprintf('Program paused. Press enter to continue.\n');
-% pause;
+x_init=30000;
+v_init=40;
+[X,~,~]=System(x_init,v_init,0,t_samp,n);
 
+Z = X+400*randn(1,n);
+
+alpha=0.2;
+beta=0.1;
+
+x_guess=30000;
+v_guess=40;
+%Filter(alpha,beta,gamma,t,z ,x_guess,v_guess,a_guess)
+[x,~,~,x_est,~,~]=Filter(alpha,beta,0,t_samp,Z,x_guess,v_guess,0);
+
+%[t./t_samp;X;Z;x;v;x_est;v_est] %#ok<NOPTS> 
+figure
+plot(t,X,t,Z,t,x,t,x_est,'linewidth',1.5);
+%plot(t,X,'linewidth',1.5);
+
+legend("True Value","Measurements","Estimates","Predictions");
+E=((X-x)./X)*100;
+figure
+plot(t,E,'linewidth',1.5);
+legend("Error Percentage");
+
+disp(find(E>epsilon,1,'last'));
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+close all;
 %% =================== Constant Acceleration ===================
 x_init=30000;
 v_init=40;
@@ -70,7 +85,7 @@ a_guess=0;
 %Filter(alpha,beta,gamma,t,z ,x_guess,v_guess,a_guess)
 [x,v,a,x_est,v_est,a_est]=Filter(alpha,beta,gamma,t_samp,Z,x_guess,v_guess,a_guess);
 
-[t./t_samp;X;Z;x;v;x_est;v_est] %#ok<NOPTS> 
+%[t./t_samp;X;Z;x;v;x_est;v_est] %#ok<NOPTS> 
 figure
 plot(t,X,t,Z,t,x,t,x_est,'linewidth',1.5);
 legend("True Value","Measurements","Estimates","Predictions");
@@ -88,6 +103,7 @@ figure
 plot(t,E,'linewidth',1.5);
 legend("Error Percentage");
 
-disp(sum(E>5));
+disp(find(E>epsilon,1,'last'));
 fprintf('Program paused. Press enter to continue.\n');
 pause;
+close all;
